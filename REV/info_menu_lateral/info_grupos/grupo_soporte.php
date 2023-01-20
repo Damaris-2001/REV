095fe855ecf1a5be364e0694363a84583d43e08d
dama <dama@dama.com>
2023-01-20 12:37:51 -0600

    mod

--- REV/info_menu_lateral/info_grupos/grupo_soporte.php
+++ REV/info_menu_lateral/info_grupos/grupo_soporte.php
@@ -17,10 +17,10 @@
     <div class="card">
         <div class="face front">
             <img src="../imagenes/Index.jpg" alt="">
-            <h3>Ingeniero de software</h3>
+            <h3>Ingenieros</h3>
         </div>
         <div class="face back">
-            <h3>Damaris</h3>
+            <h3>Nombre</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius harum molestiae iste, nihil doloribus fugiat distinctio ducimus maxime totam nulla fuga odio non aperiam eos?</p>
             <div class="link">
                 <a href="../info_menu_lateral/info_grupos/grupo_desarrollo.php">GRUPO DESARROLLO</a>
